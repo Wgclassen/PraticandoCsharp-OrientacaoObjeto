e1bb281d46389cebdd283b58abe207db85f5e473
Willian Classen <Wgclassen@gmail.com>
2025-08-25 18:33:22 -0300

    First Commit

---
+++ LibraryBookRegister/LibraryBookRegister/Livro.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryBookRegister;
+
+internal class Livro
+{
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+}
---
+++ LibraryBookRegister/LibraryBookRegister/Program.cs
@@ -0,0 +1,7 @@
+﻿using LibraryBookRegister;
+
+Livro meuLivro = new();
+meuLivro.Titulo = "Dom Casmurro";
+meuLivro.Autor = "Machado de Assis";
+
+Console.WriteLine($"Livro: {meuLivro.Titulo}\nAutor: {meuLivro.Autor}");
