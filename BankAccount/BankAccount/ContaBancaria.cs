c8b14c8bdccaf6314053f5baa972d68afd694187
Willian Classen <Wgclassen@gmail.com>
2025-08-27 18:25:26 -0300

    BankAccount

---
+++ BankAccount/BankAccount/ContaBancaria.cs
@@ -0,0 +1,12 @@
+﻿namespace BankAccount;
+
+internal class ContaBancaria(string numeroConta, decimal saldo)
+{
+    public string NumeroConta { get; set; } = numeroConta;
+    public decimal Saldo { get; set; } = saldo;
+
+    public void Depositar(decimal valor)
+    {
+        Saldo += valor;
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,6 @@
+﻿using BankAccount;
+
+ContaBancaria conta = new ContaBancaria("78901-2", 1000.00m);
+conta.Depositar(500.00m);
+
+Console.WriteLine($"Conta: {conta.NumeroConta}\nSaldo atual: {conta.Saldo}");
