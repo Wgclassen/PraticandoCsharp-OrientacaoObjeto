3b60c386aebd6fc373f975e8632a6f10ae283ec9
Willian Classen <Wgclassen@gmail.com>
2025-08-28 08:14:44 -0300

    EmployeeManagement

---
+++ EmployeeManagement/EmployeeManagement/Funcionario.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeManagement;
+
+internal class Funcionario(string nome, string cargo)
+{
+    public string Nome { get; set; } = nome;
+    public string Cargo { get; set; } = cargo;
+
+    public void Promover(string novoCargo)
+    {
+        if (this.Cargo != novoCargo)
+        {
+            this.Cargo = novoCargo;
+            Console.WriteLine("Promoção realizada com sucesso!");
+        }
+        else
+        {
+            Console.WriteLine("Erro: O novo cargo deve ser diferente do cargo atual.");
+        }
+    }
+
+}
---
+++ EmployeeManagement/EmployeeManagement/Program.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeManagement;
+
+Funcionario funcionario = new Funcionario("Carlos Pereira", "Assistente Administrativo");
+
+Console.WriteLine($"Funcionário: {funcionario.Nome}\nCargo Atual: {funcionario.Cargo}");
+
+funcionario.Promover("Assistente Administrativo");
+funcionario.Promover("Analista de Projetos");
+
+Console.WriteLine("--- Após promoção ---");
+Console.WriteLine($"Funcionário: {funcionario.Nome}\nCargo Atual: {funcionario.Cargo}");
