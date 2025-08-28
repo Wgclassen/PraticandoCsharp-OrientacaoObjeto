904f4444467b5d677c9694a6341c5952de8f0444
Willian Classen <Wgclassen@gmail.com>
2025-08-28 09:31:55 -0300

    RectangleArea

---
+++ RectangleArea/RectangleArea/Program.cs
@@ -0,0 +1,4 @@
+﻿using RectangleArea;
+
+Retangulo retangulo = new(altura: 5, largura: 8);
+Console.WriteLine($"Área do Retângulo: {retangulo.CalcularArea()}"); 
---
+++ RectangleArea/RectangleArea/Retangulo.cs
@@ -0,0 +1,12 @@
+﻿namespace RectangleArea;
+
+internal class Retangulo (int altura, int largura)
+{
+    public int Altura { get; set; } = altura;
+    public int Largura { get; set; } = largura;
+
+    public int CalcularArea()
+    {
+        return Altura * Largura;
+    }
+}
