6aee4c33a6a287fb1191f7c547738a3c0a1d7e83
Willian Classen <Wgclassen@gmail.com>
2025-08-29 07:26:44 -0300

    Movie age validation

---
+++ MovieValidation/MovieValidation/Filme.cs
@@ -0,0 +1,23 @@
+﻿namespace MovieValidation;
+
+internal class Filme(string nome, int classificacaoEtaria)
+{
+    public string Nome { get; set; } = nome;
+    public int ClassificacaoEtaria { get; set; } = classificacaoEtaria;
+
+    public bool PodeAssistir(int idadeUsuario)
+    {
+        return idadeUsuario >= this.ClassificacaoEtaria;
+    }
+
+    public void ExibirResultado(int idadeUsuario)
+    {
+        if (this.PodeAssistir(idadeUsuario))
+        {
+            Console.WriteLine($"Usuário com {idadeUsuario} anos não pode assistir ao filme {Nome}.");
+        } else
+        {
+            Console.WriteLine($"Usuário com {idadeUsuario} anos pode assistir ao filme {Nome}.");
+        }
+    }
+}
