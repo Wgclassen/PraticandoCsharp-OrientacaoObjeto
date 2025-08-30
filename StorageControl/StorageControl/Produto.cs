5022526c6eed3d94289cc174a596e5081f8cee1c
Willian Classen <Wgclassen@gmail.com>
2025-08-30 11:21:15 -0300

    StorageControl

---
+++ StorageControl/StorageControl/Produto.cs
@@ -0,0 +1,23 @@
+﻿namespace StorageControl;
+internal class Produto(string nome, int quantidadeEstoque)
+{
+    public string Nome { get; set; } = nome;
+    private int QuantidadeEstoque { get; set; } = quantidadeEstoque;
+
+    public void Retirar(int quantidade)
+    {
+        if(quantidade <= QuantidadeEstoque)
+        {
+            QuantidadeEstoque -= quantidade;
+            Console.WriteLine($"Retirada de {quantidade} unidades realizada com sucesso.");
+        } else
+        {
+            Console.WriteLine($"Erro: Estoque insuficiente para retirada de {quantidade} unidades.");
+        }
+    }
+
+    public void ExibirEstoque()
+    {
+        Console.WriteLine($"Produto: {Nome}\nEstoque: {QuantidadeEstoque}");
+    }
+}
---
+++ StorageControl/StorageControl/Program.cs
@@ -0,0 +1,6 @@
+﻿using StorageControl;
+
+Produto produto = new Produto("Caneta Azul", 20);
+produto.Retirar(5);
+produto.ExibirEstoque();
+produto.Retirar(30);
